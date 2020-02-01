483ec0e1dd04599e0801bea8f36664c15c7db35e
Mayan <mshoshan@uci.edu>
2020-02-01 00:54:43 -0800

    fix vision

--- Neon Stealth/Assets/script/lightDetection.cs
+++ Neon Stealth/Assets/script/lightDetection.cs
@@ -8,7 +8,6 @@ public class lightDetection : MonoBehaviour
     public LayerMask viewM;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -16,23 +15,55 @@ public class lightDetection : MonoBehaviour
     {
         
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Vector3 raycastDir = (other.transform.position - transform.position);
+            raycastDir.Normalize();
+            RaycastHit[] coverHits;
 
+            coverHits = Physics.RaycastAll(transform.position, raycastDir, 2.9f);
+
+            if (coverHits.Length <= 2)
+            {
+                other.gameObject.GetComponent<ControllerPlayer>().kill();
+            }
+            if (coverHits.Length > 0)
+            {
+
+                for (int i = 0; i < coverHits.Length; i++)
+                {
+                    Debug.Log(coverHits[i].transform.gameObject);
+                }
+
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            Vector3 raycastDir = (other.transform.position - transform.position);
+            raycastDir.Normalize();
+            RaycastHit[] coverHits;
 
-            RaycastHit hit;
-            if (!Physics.Linecast(other.transform.position, transform.position,out hit, viewM)) {
-                if (hit.transform.gameObject.tag == "Player") {
-                    hit.transform.gameObject.GetComponent<ControllerPlayer>().kill();
-                }
-              
+            coverHits = Physics.RaycastAll(transform.position, raycastDir, 2.9f);
 
-                Debug.Log("hit");
+            if (coverHits.Length <= 2)
+            {
+                other.gameObject.GetComponent<ControllerPlayer>().kill();
             }
-            
+            if (coverHits.Length > 0)
+            {
+
+                for (int i = 0; i < coverHits.Length; i++) {
+                    Debug.Log(coverHits[i].transform.gameObject);
+                }
+
+            }
         }
+
     }
 }
